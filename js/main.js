b6445bb44992ec18660500a0ba6541e4bb9c562f
Pawel Parafianowicz <pawelp@outlook.com>
2017-02-15 18:49:58 -0500

    assignment complete

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+$(function() {
+  $('.switch').on('click', function() {
 
+  $('body').toggleClass('dark');
+  $('body').toggleClass('light');
 
+  if ($('.switch button').attr('class') === 'on') {
+    $('.switch button').removeClass('on').addClass('off');
+    $('.status').text("Hey, who turned out the lights?");
+  } else {
+    $('.switch button').removeClass('off').addClass('on');
+    $('.status').text("It's so bright in here!");
+  }
+  })
+ });
